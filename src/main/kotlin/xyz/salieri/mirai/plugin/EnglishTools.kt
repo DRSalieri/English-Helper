e8da7e8d54b6322ab1bf290d29dc2dce76ac615b
Zhenyu Yan <1015198808@qq.com>
2021-08-09 14:56:29 +0800

    使用表驱动代替 if-else 嵌套。

--- src/main/kotlin/xyz/salieri/mirai/plugin/EnglishTools.kt
+++ src/main/kotlin/xyz/salieri/mirai/plugin/EnglishTools.kt
@@ -7,22 +7,22 @@ import xyz.salieri.english.type.Word
 import xyz.salieri.english.type.defaultword
 import xyz.salieri.mirai.plugin.bookData
 
+val books: Map<String, WordTable> = mapOf(
+    "CET4" to bookData.BookCET4,
+    "CET6" to bookData.BookCET6,
+    "TOEFL" to bookData.BookTOEFL,
+    "IELTS" to bookData.BookIELTS,
+    "SAT" to bookData.BookSAT,
+    "GRE" to bookData.BookGRE,
+    "KAOYAN" to bookData.BookKAOYAN,
+)
 fun randomword(name: String): Word{
-    if(name == "CET4")
-        return bookData.BookCET4.values.random()
-    else if(name == "CET6")
-        return bookData.BookCET6.values.random()
-    else if(name == "TOEFL")
-        return bookData.BookTOEFL.values.random()
-    else if(name == "IELTS")
-        return bookData.BookIELTS.values.random()
-    else if(name == "SAT")
-        return bookData.BookSAT.values.random()
-    else if(name == "GRE")
-        return bookData.BookGRE.values.random()
-    else if(name == "KAOYAN")
-        return bookData.BookKAOYAN.values.random()
-    else return defaultword
+    val book = books[name]
+    if(book != null) {
+        return book.values.random()
+    } else {
+        return defaultword
+    }
 }
 // 将单词转化为题目
 fun wordToQuestion(index: Int,total: Int,word: Word,timeLim: Long): String{
