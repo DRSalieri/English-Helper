56acbc704ebc3cc46e7cd2904821f3a18a934d66
Zhenyu Yan <1015198808@qq.com>
2021-08-18 10:48:30 +0800

    Add handling of exception.

--- src/main/kotlin/xyz/salieri/mirai/plugin/command/EnglishRandCommand.kt
+++ src/main/kotlin/xyz/salieri/mirai/plugin/command/EnglishRandCommand.kt
@@ -19,14 +19,17 @@ object EnglishRandCommand : SimpleCommand(
 ){
     @Handler
     suspend fun CommandSenderOnMessage<*>.handle(book: String) {
-        val t: Word = randomword(book)
-        sendMessage(
-            buildMessageChain {
-                appendLine("${t.word}")
-                t.trans.forEach{
-                    appendLine("[${it.pos}]  ${it.tran}")
+        try {
+            val t: Word = randomword(book)
+            sendMessage( t.word +
+                t.trans.joinToString("\n") {
+                    "[${it.pos}]  ${it.tran}"
                 }
-            }
-        )
+            )
+        } catch (e: java.io.FileNotFoundException) {
+            sendMessage(
+                "不存在${book}这本单词书，目前存在的单词书有：\n" + getBooks()
+            )
+        }
     }
 }
